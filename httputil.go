da174966e7cf57b29c9bf7a18714fdeca0d1c5d2
morrildl <morrildl@playground.global>
2018-05-11 18:50:07 -0700

    Add a security-hardened HTTP server helper.

--- httputil.go
+++ httputil.go
@@ -17,8 +17,10 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 
 	"playground/log"
+	"playground/session"
 )
 
 // Send writes the indicated data to the client as the indicated content-type, handling
@@ -243,34 +245,219 @@ func CheckAPISecret(req *http.Request) bool {
 // If Config.APISecretValue is set (i.e. via config.json), the wrapper optionally enforces the API
 // secret.
 func HandleFunc(path string, methods []string, handler http.HandlerFunc) {
-	http.HandleFunc(path, func(writer http.ResponseWriter, req *http.Request) {
-		defer func() {
-			if r := recover(); r != nil {
-				log.Warn(path, fmt.Sprintf("panic in handler for %s %s", req.Method, req.URL.Path), r)
-				SendJSON(writer, http.StatusInternalServerError, struct{}{})
+	handler = Wrapper().WithPanicHandler().WithSecretSentry().WithMethodSentry(methods).Wrap(handler)
+	http.HandleFunc(path, handler)
+}
+
+// NewHardenedTLSConfig returns a *tls.Config that enables only modern, PFS-permitting ciphers.
+func NewHardenedTLSConfig() *tls.Config {
+	return &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384, // doesn't provide PFS
+			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256, // doesn't provide PFS
+		},
+	}
+}
+
+// HardenedServer is a thinly-wrapped *http.Server that adds some convenience methods for starting
+// servers in a more secure configuration than the Go defaults. Based on
+// https://blog.cloudflare.com/exposing-go-on-the-internet/
+type HardenedServer struct {
+	*http.Server
+	bindInterface string
+	port          int
+}
+
+// NewHardenedServer returns a HardenedServer (i.e. *http.Server) with timeout and TLS
+// configurations suitable for secure serving. The TLSConfig in the returned instance is a
+// HardenedTLSConfig as above. The server's Handler is set to a fresh *http.ServeMux instance, which
+// is also returned.
+func NewHardenedServer(bindInterface string, port int) (*HardenedServer, *http.ServeMux) {
+	mux := http.NewServeMux()
+	return &HardenedServer{
+		Server: &http.Server{
+			Addr:         fmt.Sprintf("%s:%d", bindInterface, port),
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  120 * time.Second,
+			TLSConfig:    NewHardenedTLSConfig(),
+			Handler:      mux,
+		},
+		bindInterface: bindInterface,
+		port:          port,
+	}, mux
+}
+
+// ListenAndServeHSTS starts up an unencrypted HTTP server whose only function is to redirect all
+// URLs to the HTTPS server. The HTTP Strict Transport Security header is set in these redirects to
+// ensure compatible browsers default to the HTTPS instance for future requests.
+func (s *HardenedServer) ListenAndServeHSTS(httpsHost string, httpPort int) {
+	if httpPort < 1 {
+		panic(fmt.Sprintf("invalid HSTS port %d specified", httpPort))
+	}
+	if httpsHost == "" {
+		httpsHost = s.bindInterface
+	}
+	if s.port != 443 {
+		httpsHost = fmt.Sprintf("%s:%d", httpsHost, s.port)
+	}
+	go func() {
+		log.Warn("main (http)", "fallback HTTP server shutting down", (&http.Server{
+			Addr:         fmt.Sprintf("%s:%d", s.bindInterface, httpPort),
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 5 * time.Second,
+			IdleTimeout:  120 * time.Second,
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				w.Header().Set("Connection", "close")
+				w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+				url := fmt.Sprintf("https://%s/%s", httpsHost, req.URL.String())
+				log.Debug("main (http)", "redirect to https", url)
+				http.Redirect(w, req, url, http.StatusMovedPermanently)
+			}),
+		}).ListenAndServe())
+	}()
+}
+
+// RequireClientRoot instructs the HardenedServer to only accept connections from clients which
+// present a client certificate signed by a CA during TLS handshake. If the provided rootCertFile is
+// a specific (self-signed) certificate instead of a CA certificate, the behavior is basically
+// certificate pinning. This is intended for use in API servers where the only clients are
+// non-browser entities.
+func (s *HardenedServer) RequireClientRoot(rootCertFile string) {
+	rootCert, err := ioutil.ReadFile(rootCertFile)
+	if err != nil {
+		panic(err)
+	}
+
+	clientRoot := x509.NewCertPool()
+	clientRoot.AppendCertsFromPEM(rootCert)
+	s.TLSConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	s.TLSConfig.ClientCAs = clientRoot
+	s.TLSConfig.BuildNameToCertificate()
+}
+
+type wrapper struct {
+	cur  func(http.HandlerFunc) http.HandlerFunc
+	prev *wrapper
+}
+
+// Wrapper returns a builder which can be used to assemble a client request authentication strategy
+// from a selection of boilerplate building blocks. Calling the other methods on this object
+// constructs a call chain of authentication operators, which can then be use to Wrap() a standard
+// http.HandlerFunc. This allows request handlers to refrain from repeating common authentication
+// code blocks.
+//
+// mux.HandleFunc(
+//   "/some/path",
+//   httputil.Wrapper()
+//     .WithPanicHandler()
+//     .WithSecretSentry()
+//     .WithSessionSentry(nil)
+//     .WithMethodSentry([]string{"GET", "PUT"})
+//     .Wrap(somePathHandler))
+func Wrapper() *wrapper {
+	return &wrapper{}
+}
+
+func (w *wrapper) prep(f func(http.HandlerFunc) http.HandlerFunc) *wrapper {
+	w.cur = f
+	next := &wrapper{prev: w}
+	return next
+}
+
+// Wrap constructs a final http.HandlerFunc out of the chain of authenticator blocks represented by
+// w.
+func (w *wrapper) Wrap(f http.HandlerFunc) http.HandlerFunc {
+	if w.prev == nil {
+		return w.cur(f)
+	}
+	return w.prev.Wrap(w.cur(f))
+}
+
+// WithMethodSentry adds a request method check to the chain represented by w. It compares the
+// current request method against the provided list of messages, and aborts the request with an
+// error if the method is not approved. This is intended to ensure that REST endpoint handlers don't
+// have to deal with methods they aren't expecting.
+func (w *wrapper) WithMethodSentry(methods []string) *wrapper {
+	return w.prep(func(f http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, req *http.Request) {
+			allowed := false
+			for _, method := range methods {
+				if method == req.Method {
+					allowed = true
+					break
+				}
 			}
-		}()
-
-		if !CheckAPISecret(req) {
-			log.Warn(path, "API secret check failed", req.URL.Path, req.Method)
-			SendJSON(writer, http.StatusForbidden, struct{}{})
-			return
-		}
-
-		allowed := false
-		for _, method := range methods {
-			if method == req.Method {
-				allowed = true
-				break
+			if !allowed {
+				log.Warn("methodSentry", "disallowed HTTP method", req.URL.Path, req.Method)
+				SendJSON(writer, http.StatusMethodNotAllowed, struct{}{})
+				return
 			}
+			f(writer, req)
 		}
-		if !allowed {
-			log.Warn(path, "disallowed HTTP method", req.URL.Path, req.Method)
-			SendJSON(writer, http.StatusMethodNotAllowed, struct{}{})
-			return
-		}
+	})
+}
 
-		log.Status(path, fmt.Sprintf("%s %s", req.Method, req.URL.Path))
-		handler(writer, req)
+// WithPanicHandler adds a top-level defer handler for panics. Requests that trip this handler
+// return a 500 Internal Server Error response. This allows handlers to avoid cluttering their code
+// with lots of `if err != nil` checks for internal issues, like database errors or filesystem errors.
+func (w *wrapper) WithPanicHandler() *wrapper {
+	return w.prep(func(f http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, req *http.Request) {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Warn("panicHandler", fmt.Sprintf("panic in handler for %s %s", req.Method, req.URL.Path), r)
+					SendJSON(writer, http.StatusInternalServerError, struct{}{})
+				}
+			}()
+			f(writer, req)
+		}
+	})
+}
+
+// WithSecretSentry adds a check for an API secret in the request header. The header key and value
+// must match those specified in the module's Config struct. If the header is missing or invalid,
+// a 403 response is returned to the client.
+func (w *wrapper) WithSecretSentry() *wrapper {
+	return w.prep(func(f http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, req *http.Request) {
+			if !CheckAPISecret(req) {
+				log.Warn("secretSentry", "API secret check failed", req.URL.Path, req.Method)
+				SendJSON(writer, http.StatusForbidden, struct{}{})
+				return
+			}
+			f(writer, req)
+		}
+	})
+}
+
+// WithSessionSentry adds a check for OAuth2 login. See the `playground/session` package for
+// details.
+func (w *wrapper) WithSessionSentry(body interface{}) *wrapper {
+	return w.prep(func(f http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, req *http.Request) {
+			ssn := session.GetSession(req)
+			if !ssn.IsLoggedIn() {
+				ssn.Update(writer)
+				if body != nil {
+					SendJSON(writer, http.StatusForbidden, body)
+				} else {
+					SendPlaintext(writer, http.StatusForbidden, "Unauthenticated")
+				}
+			}
+			f(writer, req)
+		}
 	})
 }
